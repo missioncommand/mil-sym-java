9e7122d97c2e068362fdd076e66805535b0e7837
C5ISR\Michael.P.Spinelli <michael.p.spinelli6.civ@army.mil>
2025-08-15 10:52:08 -0400

    Fix for: Oceanographic / Bottom Feature - essentially italic serif fonts need more vertical space when generating SVGs pixel sizes above 150 are fine, which is weird.  The preprocessed measurements are accurate and this isn't an issue with other symbols.

--- src/main/java/armyc2/c5isr/renderer/SinglePointSVGRenderer.java
+++ src/main/java/armyc2/c5isr/renderer/SinglePointSVGRenderer.java
@@ -659,6 +659,16 @@ public class SinglePointSVGRenderer {
                 if (outlineSymbol) {
                     borderPadding = RendererUtilities.findWidestStrokeWidth(siIcon.getSVG());
                 }
+
+                //Oceanographic / Bottom Feature - essentially italic serif fonts need more vertical space
+                //pixel sizes above 150 it's fine, which is weird
+                if(SymbolUtilities.getBasicSymbolID(symbolID).startsWith("461206"))
+                {
+                    double va = siIcon.getBbox().getHeight() * 0.025;
+                    double ha = siIcon.getBbox().getWidth() * 0.025;//some also need to be slightly wider
+                    siIcon.getBbox().setRect(siIcon.getBbox().getX(),siIcon.getBbox().getY() - va,siIcon.getBbox().getWidth() + ha,siIcon.getBbox().getHeight() + va);
+                }
+
                 top = (int)Math.floor(siIcon.getBbox().getY());
                 left = (int)Math.floor(siIcon.getBbox().getX());
                 width = (int)Math.ceil(siIcon.getBbox().getWidth() + (siIcon.getBbox().getX() - left));
