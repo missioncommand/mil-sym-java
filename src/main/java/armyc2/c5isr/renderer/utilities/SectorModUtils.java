525e715f6116107624f438da3d7ce6b3ef7c3161
C5ISR\Michael.P.Spinelli <michael.p.spinelli6.civ@army.mil>
2026-02-23 13:45:13 -0500

    fixes for getName and getSectorModList

--- src/main/java/armyc2/c5isr/renderer/utilities/SectorModUtils.java
+++ src/main/java/armyc2/c5isr/renderer/utilities/SectorModUtils.java
@@ -161,9 +161,11 @@ public class SectorModUtils
     public ArrayList<String[]> getSectorModList(int version, int symbolSet, int location)
     {
         StringBuilder sb = new StringBuilder();
-        int ver = SymbolID.Version_2525Dch1;
-        if(version >= SymbolID.Version_2525E )
+        int ver = version;
+        if(version == SymbolID.Version_2525E)
             ver = SymbolID.Version_2525Ech1;
+        if(version == SymbolID.Version_APP6Ech1)
+            ver = SymbolID.Version_APP6Ech2;
 
         int ss = symbolSet;
         if (ss > 50 && ss < 60)
@@ -192,9 +194,11 @@ public class SectorModUtils
     public String getName(int version, int symbolSet, int location, String code)
     {
         StringBuilder sb = new StringBuilder();
-        int ver = SymbolID.Version_2525Dch1;
-        if(version >= SymbolID.Version_2525E )
+        int ver = version;
+        if(version == SymbolID.Version_2525E)
             ver = SymbolID.Version_2525Ech1;
+        if(version == SymbolID.Version_APP6Ech1)
+            ver = SymbolID.Version_APP6Ech2;
 
         int ss = symbolSet;
         if (ss > 50 && ss < 60)
